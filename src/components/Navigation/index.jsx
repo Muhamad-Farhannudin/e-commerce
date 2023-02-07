b6acb2273ffa6ccf31f00d23e7aa778d1e01e58d
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-02-07 19:21:44 +0700

    menambahkan hover pada Navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -9,14 +9,14 @@ export default function index() {
         <div className='flex items-center justify-between w-1/2 group'>
             <div className='flex items-center w-1/5 justify-evenly group-hover:cursor-pointer'>
                 <MdMenu />
-                <p className='font-medium text-base text-dark'>All category</p>
+                <p className='font-medium text-base hover:opacity-70 hover:duration-300 hover:transition text-dark'>All category</p>
             </div>
-            <p className='font-medium text-base text-dark group-hover:cursor-pointer'>Hot offers</p>
-            <p className='font-medium text-base text-dark group-hover:cursor-pointer'>Gift boxes</p>
-            <p className='font-medium text-base text-dark group-hover:cursor-pointer'>Projects</p>
-            <p className='font-medium text-base text-dark group-hover:cursor-pointer'>Menu item</p>
+            <p className='font-medium text-base hover:opacity-50 hover:duration-300 hover:transition text-dark group-hover:cursor-pointer'>Hot offers</p>
+            <p className='font-medium text-base hover:opacity-50 hover:duration-300 hover:transition text-dark group-hover:cursor-pointer'>Gift boxes</p>
+            <p className='font-medium text-base hover:opacity-50 hover:duration-300 hover:transition text-dark group-hover:cursor-pointer'>Projects</p>
+            <p className='font-medium text-base hover:opacity-50 hover:duration-300 hover:transition text-dark group-hover:cursor-pointer'>Menu item</p>
             <div className='flex items-center justify-between group-hover:cursor-pointer'>
-                <p className='font-medium text-base text-dark'>Help</p>
+                <p className='font-medium text-base hover:opacity-50 hover:duration-300 hover:transition text-dark'>Help</p>
                 <MdExpandMore />
             </div>
         </div>
